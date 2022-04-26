e74f5744709ff7ba15d56081014702f02cdcf757
blasecato <bl.calderon@udla.edu.co>
2022-04-25 20:21:31 -0500

    style(contact me): Home

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { Form, Input, Button } from "antd";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+} from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+const markers = [
+  { markerOffset: 15, name: "Florencia", coordinates: [-75.60419, 1.62696] },
+];
 
 const ContactMe = () => {
   const onFinish = (values: any) => {
     console.log("Success:", values);
+    const $buttonMailto = document.querySelector("#mailTo");
+    $buttonMailto.setAttribute(
+      "href",
+      `mailto:bl.calderonn@gmail.com?subject=Nombre - ${values.name}, Correo - ${values.email}&body=${values.comment}`
+    );
+    $buttonMailto.click();
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -16,30 +35,68 @@ const ContactMe = () => {
         <h1 className="title">Contact me</h1>
         <div className="cont-text">
           <p className="description">
-            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
-            aliquid fuga eum quidem. Sit sint consectetur velit. 
+            ¡Contactame y cambiemos el mundo!
           </p>
         </div>
         <div className="ContactMe__content">
           <div className="ContactMe__box">
-            <span>Hola</span>
+            <ComposableMap
+              projection="geoAzimuthalEqualArea"
+              projectionConfig={{
+                rotate: [78, -5, 0],
+                scale: 1900,
+              }}
+            >
+              <Geographies geography={geoUrl}>
+                {({ geographies }) =>
+                  geographies
+                    .filter((d) => d.properties.REGION_UN === "Americas")
+                    .map((geo) => (
+                      <Geography
+                        key={geo.rsmKey}
+                        geography={geo}
+                        fill="#4a27b4"
+                        stroke="#D6D6DA"
+                      />
+                    ))
+                }
+              </Geographies>
+              {markers.map(({ name, coordinates, markerOffset }) => (
+                <Marker key={name} coordinates={coordinates}>
+                  <g
+                    fill="none"
+                    stroke="#00dbd0"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    transform="translate(-12, -24)"
+                  >
+                    <circle cx="12" cy="10" r="3" />
+                    <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                  </g>
+                  <text
+                    textAnchor="middle"
+                    y={markerOffset}
+                    style={{ fontFamily: "system-ui", fill: "#ffffff" }}
+                  >
+                    {name}
+                  </text>
+                </Marker>
+              ))}
+            </ComposableMap>
           </div>
           <div className="ContactMe__form">
             <Form
               name="basic"
-              labelCol={{ span: 8 }}
-              wrapperCol={{ span: 16 }}
-              initialValues={{ remember: true }}
               onFinish={onFinish}
               onFinishFailed={onFinishFailed}
-              autoComplete="off"
             >
               <Form.Item
                 name="email"
                 rules={[
                   {
                     required: true,
-                    message: "Please input your Email!",
+                    message: "Por favor ingrese su email",
                     type: "email",
                   },
                 ]}
@@ -49,23 +106,30 @@ const ContactMe = () => {
 
               <Form.Item
                 name="name"
-                rules={[{ required: true, message: "Please input your Name!" }]}
+                rules={[
+                  { required: true, message: "Por favor ingrese su nombre" },
+                ]}
               >
-                <Input placeholder="Name" />
+                <Input placeholder="Nombre" />
               </Form.Item>
 
               <Form.Item
                 name="comment"
-                rules={[{ required: true, message: "Please input your Comment!" }]}
+                rules={[
+                  { required: true, message: "Por favor ingrese su mensaje" },
+                ]}
               >
-                <Input.TextArea placeholder="Comment" />
+                <Input.TextArea placeholder="Mensaje" />
               </Form.Item>
 
-              <Form.Item>
+              <Form.Item className="item-button">
                 <Button type="primary" htmlType="submit">
                   Send Message
                 </Button>
               </Form.Item>
+              <a href="mailto:bl.calderonn@gmail.com" id="mailTo">
+                hola
+              </a>
             </Form>
           </div>
         </div>
--- src/scenes/Public/Home/Home.js
+++ src/scenes/Public/Home/Home.js
@@ -5,6 +5,7 @@ import About from "../../../components/About/About.js";
 import Projects from "../../../components/Projects/Projects.js";
 import Skills from "../../../components/Skills/Skills.js";
 import ContactMe from "../../../components/ContactMe/ContactMe.js";
+import whatsaap from "../../../assets/images/whatsaap.png";
 
 const Home = () => {
   return (
@@ -17,6 +18,9 @@ const Home = () => {
         <Skills />
         <ContactMe />
       </div>
+      <a href="https://wa.me/573157308621?text=Me%20interesaria%20trabajar%20con%20tigo" className="buttonW">
+        <img src={whatsaap} alt="whatsaap" className="imgwhatsaap"/>
+      </a>
     </div>
   );
 }
